bbff91f137d6755a37a3abe1a1c0323b5216a192
okada26 <t.okada8885@gmail.com>
2024-10-30 21:00:02 +0900

    課題

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const buttun = document.getElementById('btn');
+buttun.addEventListener('click', () => {
+  const text = document.getElementById('text');
+  text.textContent = 'ボタンをクリックしました';
+  
+})
